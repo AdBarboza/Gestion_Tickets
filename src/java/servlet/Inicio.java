a6d34c01b1d62bbe477988ec2c1a76debfdeb3e4
arroyo-piedra <40010920+arroyo-piedra@users.noreply.github.com>
2018-06-08 22:08:26 -0600

    Primera pantalla terminada

--- src/java/servlet/Inicio.java
+++ src/java/servlet/Inicio.java
@@ -31,19 +31,17 @@ public class Inicio extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ConsultaEstudiante</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ConsultaEstudiante at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+           
+            if (request.getParameter("btnBuscar") != null){
+                /*atención al boton de Registro*/
+                /*redirecciona al servlet para que despliegue otra pagina*/
+                response.sendRedirect("PantallaBuscar.html");
+            }
+            else{
+                if (request.getParameter("btnBuscar") != null){
+                    response.sendRedirect("PantallaBuscar.html");
+                }
+            }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
